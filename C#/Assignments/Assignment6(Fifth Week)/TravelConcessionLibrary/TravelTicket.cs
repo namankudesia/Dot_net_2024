1a34476441a88cb3a837620870a035a907cac9b2
namanku <namanku@DESKTOP-I8GHI2M>
2024-07-08 13:18:27 +0530

    Assignment 6

---
+++ C#/Assignments/Assignment6(Fifth Week)/TravelConcessionLibrary/TravelTicket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelConcessionLibrary
+{
+    public class TravelTicket
+    {
+        public const decimal TotalFare = 500m;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string CalculateConcession()
+        {
+            if (Age <= 5)
+            {
+                return "Little Champs - Free Ticket";
+            }
+            else if (Age > 60)
+            {
+                decimal concessionAmount = TotalFare * 0.3m;
+                decimal discountedFare = TotalFare - concessionAmount;
+                return $"Senior Citizen - Fare: {discountedFare:C}";
+            }
+            else
+            {
+                return $"Ticket Booked - Fare: {TotalFare:C}";
+            }
+        }
+    }
+}

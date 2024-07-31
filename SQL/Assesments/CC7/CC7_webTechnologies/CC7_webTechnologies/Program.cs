e9c129aad542a308e3e0a8b8f96a968a33172869
namanku <namanku@DESKTOP-I8GHI2M>
2024-07-31 11:16:35 +0530

    CC7

---
+++ SQL/Assesments/CC7/CC7_webTechnologies/CC7_webTechnologies/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC7_webTechnologies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

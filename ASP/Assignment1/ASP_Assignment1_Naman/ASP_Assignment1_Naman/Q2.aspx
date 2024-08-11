417b964ea9a6e8ed919bb27d943aa193d4d8bc48
namanku <namanku@DESKTOP-I8GHI2M>
2024-08-11 11:33:45 +0530

    Assignment 1 ASP

---
+++ ASP/Assignment1/ASP_Assignment1_Naman/ASP_Assignment1_Naman/Q2.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Q2.aspx.cs" Inherits="ASP_Assignment1_Naman.Q2" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Fruit Selector</title>
+    <style>
+        .fruit-container {
+            text-align: center;
+            padding: 20px;
+        }
+        .fruit-image {
+            width: 200px;
+            height: 200px;
+            border-radius: 10px;
+        }
+        .fruit-details {
+            margin-top: 20px;
+            font-size: 20px;
+            font-weight: bold;
+        }
+        .fruit-selection {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body style="height: 393px">
+    <form id="fruitForm" runat="server" class="fruit-container">
+        <div class="fruit-selection">
+            <asp:Label ID="lblSelectFruit" runat="server" Text="Select a Fruit: "></asp:Label>
+            <asp:DropDownList ID="ddlFruits" runat="server">
+                <asp:ListItem Text="--Select Fruit--" Value="" />
+                <asp:ListItem Text="Apple" Value="Apple" />
+                <asp:ListItem Text="Banana" Value="Banana" />
+                <asp:ListItem Text="Orange" Value="Orange" />
+                <asp:ListItem Text="Guava" Value="Guava" />
+                <asp:ListItem Text="Mango" Value="Mango" />
+                <asp:ListItem Text="Pomegranate" Value="Pomegranate" />
+                <asp:ListItem Text="Pineapple" Value="Pineapple" />
+            </asp:DropDownList>
+        </div>
+        <asp:Image ID="imgFruit" runat="server" CssClass="fruit-image" />
+        <div class="fruit-details">
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Check Price" />
+            <br />
+            <br />
+            <asp:Label ID="lblPrice" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/Assignment1/ASP_Assignment1_Naman/ASP_Assignment1_Naman/Q2.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace ASP_Assignment1_Naman
+{
+    public partial class Q2 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblPrice.Text = string.Empty;
+                imgFruit.ImageUrl = string.Empty;
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string selectedFruit = ddlFruits.SelectedValue;
+            SetFruitDetails(selectedFruit);
+        }
+
+        private void SetFruitDetails(string fruit)
+        {
+            string imagePath = string.Empty;
+            string price = string.Empty;
+            string bgColor = string.Empty;
+            switch (fruit)
+            {
+                case "Apple":
+                    imagePath = "~/Apple.jpg";
+                    price = "200 rupees per kg";
+                    bgColor = "#FF0800";
+                    break;
+                case "Banana":
+                    imagePath = "~/Banana.jpg";
+                    price = "60 rupees per dozen";
+                    bgColor = "#FFE135";
+                    break;
+                case "Orange":
+                    imagePath = "~/Orange.jpg";
+                    price = "180 rupees per kg";
+                    bgColor = "#FFA500";
+                    break;
+                case "Guava":
+                    imagePath = "~/Guava.jpg";
+                    price = "120 rupees per kg";
+                    bgColor = "#7FFF00";
+                    break;
+                case "Mango":
+                    imagePath = "~/Mango.jpg";
+                    price = "150 rupees per kg";
+                    bgColor = "#FFEB3B"; 
+                    break;
+                case "Pomegranate":
+                    imagePath = "~/Pomegranate.jpg";
+                    price = "250 rupees per kg";
+                    bgColor = "#DC143C"; 
+                    break;
+                case "Pineapple":
+                    imagePath = "~/Pineapple.jpg";
+                    price = "80 rupees per kg";
+                    bgColor = "#FFD700";
+                    break;
+                default:
+                    price = string.Empty;
+                    imgFruit.ImageUrl = string.Empty;
+                    bgColor = "#FFFFFF";
+                    break;          
+            imgFruit.ImageUrl = imagePath;
+            lblPrice.Text = price;
+            fruitForm.Style["background-color"] = bgColor;
+        }
+    }
+}
